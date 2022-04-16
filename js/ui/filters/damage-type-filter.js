1f43a62205031154e75f19b879428bc5266feaf8
Poulton <poultont@evam201736.b2b.regn.net>
2022-04-15 22:47:59 -0400

    Updated damage type filter to exclude split damage weapons

--- js/ui/filters/damage-type-filter.js
+++ js/ui/filters/damage-type-filter.js
@@ -6,7 +6,7 @@ class DamageTypeFilters extends UIFilters {
 
     addFiltersToPage() {
         let phys = damageTypes[0];
-        this.addFilterToPage(phys, phys, true, 'Turning this off will filter out weapons that only do physical damage');
+        this.addFilterToPage(phys, phys, true, 'Turning this off will filter out weapons that only do physical damage. If this is the only filter on, then not split damage weapons will be shown.');
         elementalDamageTypes.forEach(elementalDamageType => {
             this.addFilterToPage(elementalDamageType, elementalDamageType, true);
         });
@@ -20,12 +20,21 @@ class DamageTypeFilters extends UIFilters {
                  return (weaponDamage[damageType].total > 0);
             });
 
+            // This is temporary because if you don't meet the requirements right now, all damage is set to 0
+            if (weaponDamageTypes.length === 0) {
+                return false;
+            } else if (damageTypeFilters.length === 1 && damageTypeFilters[0] === 'Physical' && weaponDamageTypes.length > 1) {
+                return false;
+            }
+
             let include = false;
             damageTypeFilters.forEach(damageType => {
                 if (weaponDamageTypes.includes(damageType)) {
                     include = true;
                 }
             });
+
+            
             return include;
         });
     }
